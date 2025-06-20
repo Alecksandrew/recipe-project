36375f10a50de71e8be3d62e1ae286eaa3f78d93
Alecksandrew <alecksandrewcontato@gmail.com>
2025-06-19 22:49:30 -0300

    Estrutura jsx da página RecipeDetails finalizada
    - Além do jsx, adicionei um pouco do css, ainda falta
    refinar!

--- src/components/dataInColumn/dataInColumn.jsx
+++ src/components/dataInColumn/dataInColumn.jsx
@@ -1,6 +1,6 @@
 import "./dataInColumn.css";
 
-function dataInColumn({title, titlePriority, arrayOptionsKeyValue = [["Option 1", "Result 1"], ["Option 2"]], hrLineAfterOption = [], listStyle = "disc"}) {
+function dataInColumn({title, titlePriority = 2, arrayOptionsKeyValue = [["Option 1", "Result 1"], ["Option 2"]], hrLineAfterOption = [], listStyle = "disc"}) {
 
     const HeadingTag = `h${titlePriority}`
 
@@ -10,13 +10,13 @@ function dataInColumn({title, titlePriority, arrayOptionsKeyValue = [["Option 1"
                 if ( hrLineAfterOption.includes(index)) {
                     return (
                         <React.Fragment key={index}>
-                          <li><div className="item-content"><span>{KeyValue[0]}</span>{KeyValue[1] && <span>{KeyValue[1]}</span>}</div></li>
+                          <li><div className="item-content"><span>{KeyValue[0]}</span>{KeyValue[1] && <span><b>{KeyValue[1]}</b></span>}</div></li>
                           <hr />
                         </React.Fragment>
                     )
                 }
                 else {
-                    return <li key={index}><div className="item-content"><span>{KeyValue[0]}</span>{KeyValue[1] && <span>{KeyValue[1]}</span>}</div></li>
+                    return <li key={index}><div className="item-content"><span>{KeyValue[0]}</span>{KeyValue[1] && <span style={{textAlign: "right", whiteSpace: "pre-line"}}><b>{KeyValue[1]}</b></span>}</div></li>
                 }
             })
         
--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -6,13 +6,32 @@ import { useLocation } from 'react-router-dom';
 function RecipeDetails() {
     const location = useLocation();
     const recipeData = location.state;
+    console.log(recipeData);
+
+    const organizedIngredients = recipeData.extendedIngredients.map(ingredientInfo => {
+        return [ingredientInfo.original]
+    });
+
+    const organizedInstructions = recipeData.analyzedInstructions[0].steps.map(step => {
+        return [step.step]
+    })
+
+    const organizedMoreInfos = [
+        ["Dish type", `${recipeData.dishTypes[0]}`],
+        ["Ready in:", `${recipeData.readyInMinutes} min`], 
+        ["Servings", `${recipeData.servings}`], 
+        ["Nationality", `${recipeData.cuisine}`], 
+        ["Restrictions", `${recipeData.diets.join(" /\n ")}`]
+];
 
     return (
         <>
             <h1>{recipeData.title}</h1>
-            <p dangerouslySetInnerHTML={{__html: recipeData.summary}}></p>
+            <p className="description" dangerouslySetInnerHTML={{__html: recipeData.summary}}></p>
             <img src={`${recipeData.image}`} alt={`Imagem do ${recipeData.title}`} />
-            <DataInColumn title={"Ingredientes"} titlePriority={2} />
+            <DataInColumn title={"Ingredients"} arrayOptionsKeyValue={organizedIngredients}/>
+            <DataInColumn title={"Instructions"} arrayOptionsKeyValue={organizedInstructions}/>
+            <DataInColumn title={"More infos"} arrayOptionsKeyValue={organizedMoreInfos}/>
         </>
 
 
