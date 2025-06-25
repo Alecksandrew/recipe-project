da1917f72c8d57574280e9544f7d8bd5b005ba8a
Alecksandrew <alecksandrewcontato@gmail.com>
2025-06-25 18:05:23 -0300

    Logic to show more or less of description was fixed
    - Now even if there is a short or no description,
    the feature will work correctly.

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -1,12 +1,12 @@
 import DataInColumn from "../components/dataInColumn/dataInColumn";
 import styles from "./RecipeDetails.module.css"
 import { useLocation } from 'react-router-dom';
+import { useRef, useState, useEffect } from 'react';
 
 
 function RecipeDetails() {
     const location = useLocation();
     const recipeData = location.state;
-    console.log(recipeData);
 
     const ingredients = recipeData.extendedIngredients;
     const organizedIngredients = ingredients.map(ingredientInfo => {
@@ -34,14 +34,54 @@ function RecipeDetails() {
         ["Servings", `${recipeData.servings}`], 
         ["Nationality", `${recipeData.cuisine}`], 
         ["Restrictions", `${recipeData.diets.join(" /\n ")}`]
-];
+
+    ];
+
+
+    // Logic to show more or less of the description
+    const readMoreButton = useRef(null);
+    const descriptionTag = useRef(null);
+    const [ isExpanded, setIsExpanded ] = useState(false);
+
+    useEffect(() => {
+        const isOverflowing = descriptionTag.current.scrollHeight > descriptionTag.current.clientHeight;
+        
+        isOverflowing ? readMoreButton.current.style.display = "block" : readMoreButton.current.style.display = "none";
+    }, []);
+
+    function handleReadMoreClick() {
+        if (!isExpanded) {
+            readMoreButton.current.innerText = "Read Less";
+            descriptionTag.current.style.cssText = `
+                display: block;
+                -webkit-line-clamp: unset;
+                line-clamp: unset;
+                -webkit-box-orient: unset;
+                overflow: visible;
+                text-overflow: unset;
+            `;
+        }
+        else {
+            readMoreButton.current.innerText = "Read More";
+            descriptionTag.current.style.cssText = `
+                display :-webkit-box;
+                -webkit-line-clamp: 3;
+                line-clamp: 3;
+                -webkit-box-orient: vertical;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            `;
+        }
+
+        setIsExpanded(!isExpanded) 
+    }
 
     return (
         <div className={styles.containerWholePage}>
             <div className={styles.containerMainContent}>
                 <h1 className={styles.h1}>{recipeData.title}</h1>
-                <p className={styles.description} dangerouslySetInnerHTML={{__html: recipeData.summary}}></p>
-                <button className={styles.btnReadMore} >Read More</button>
+                <p className={styles.description} dangerouslySetInnerHTML={{__html: recipeData.summary}} ref={descriptionTag}></p>
+                <button className={styles.btnReadMore} ref={readMoreButton} onClick={handleReadMoreClick}>Read More</button>
             </div>
             <div className={styles.containerFoodImage}>
                 <img className={styles.foodImage} src={`${recipeData.image}`} alt={`Imagem do ${recipeData.title}`} />
