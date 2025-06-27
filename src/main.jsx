5351209cbfec91cee46a4ee09a5c5f3242bc4394
Alecksandrew <alecksandrewcontato@gmail.com>
2025-06-27 20:38:18 -0300

    Fixing routers problem in deploy
    - When use reload page, erro page is shown

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import Home from "./pages/Home.jsx"
 import RecipeDetails from './pages/RecipeDetails.jsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App/>,
