c12a0081d454ea72b08094c2e7f88150e6c751ae
Alecksandrew <alecksandrewcontato@gmail.com>
2025-06-28 15:28:49 -0300

    Logic to autocomplet ingredients when the user write

--- src/pages/WhatsInYourKitchen.jsx
+++ src/pages/WhatsInYourKitchen.jsx
@@ -1,32 +1,102 @@
 import styles from "./WhatsInYourKitchen.module.css"
 import { FaSearch } from "react-icons/fa";
+import { useState, useEffect, useRef } from "react"
 import DropdownIngredients from "../components/DropdownIngredients/DropdownIngredients";
 
 
 
 function WhatsInYourKitchen({placeholderSearch}) {
-
-
     
+    const [autocompleteData, setAutocompleteData] = useState(null);
+    const [searchValue, setSearchValue] = useState(null);
+    const [ selectedIngredients, setSelectedIngredients] = useState(null);
+    const [ isLoading, setIsLoading] = useState(false);
+
+    console.log(searchValue)
+    console.log(autocompleteData)
+
+    useEffect(() => {
+        async function autocompleteIngredients() {
+            
+            if(!searchValue || searchValue.trim === "") {
+                setAutocompleteData(null)
+                return;
+            }
+
+            try {
+                setIsLoading(true);
+                const autocompletEndpoint = "https://api.spoonacular.com/food/ingredients/autocomplete"
+                const params = new URLSearchParams({
+                    query: searchValue,
+                    apiKey: "6b0d610fe5cf4296b3dd9023ae8150fb",
+                    number: 3
+                })
+            
+                const response = await fetch(`${autocompletEndpoint}?${params.toString()}`)
+                const data = await response.json();
+                setAutocompleteData(data);
+            }
+            catch(error) {
+                console.error(error)
+                setAutocompleteData(null) 
+            }
+            finally {
+                setIsLoading(false)
+            }
+            
+        }
+
+        autocompleteIngredients()
+        
+    }, [searchValue])
+
+
+    function listAutocompletedIngredients(data) {
+        if(!data) return null
+
+        return data.map((ingredient, index) => {
+                    if(!searchValue || searchValue.trim() ==="") return
+                    
+                    return <li key={index}>
+                                <DropdownIngredients
+                                imageURL={`https://spoonacular.com/cdn/ingredients_250x250/${ingredient.image}`}
+                                imageAlt={ingredient.name}
+                                ingredientName={ingredient.name}/>
+                            </li>
+                            
+
+                })
+    }
+
+
+
 
     return(
         <>
             <form method="get" className={styles.form}>
-                <section className={`${styles.searchBar} ${styles.section}`}>
-                    <FaSearch className={styles.searchIcon}/>
-                    <input
-                    placeholder={placeholderSearch}
-                    type="search"
-                    id="ingredientsName"
-                    name="ingredientsName"
-                    className={styles.inputSearch}
-                    autoComplete="off"
-                    />
+                <section className={styles.sectionAutocomplet}>
+                    <div className={styles.searchBar}>
+                        <FaSearch className={styles.searchIcon}/>
+                        <input
+                        placeholder={placeholderSearch}
+                        type="search"
+                        id="ingredientsName"
+                        name="ingredientsName"
+                        className={styles.inputSearch}
+                        autoComplete="off"
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        />
+                    </div>
+                        <ul className={`${styles.containerAutocompletedIngredients} ${!searchValue || searchValue.trim() === "" ? styles.hidden : ""}`}>
+                            {searchValue && autocompleteData && autocompleteData.length === 0 && !isLoading ? <li key={"notFound"} id={styles.notFound}>Ingredient not found!</li> : 
+                            listAutocompletedIngredients(autocompleteData)}
+                        </ul>
+
                 </section>
                 <section className={styles.section}>
                     <h2>Selected ingredients</h2>
                     <ul style={styles.selectedIngredientsContainer}>
-                        <li>Each selected ingredient</li>
+                        
                     </ul>
                 </section>
                 <section className={styles.section}>
@@ -42,8 +112,6 @@ function WhatsInYourKitchen({placeholderSearch}) {
                 <button type="submit">Search recipes</button>
             </form>
             <h1>Selected recipes</h1>
-           <DropdownIngredients/>
-            
         </>
     )
 
